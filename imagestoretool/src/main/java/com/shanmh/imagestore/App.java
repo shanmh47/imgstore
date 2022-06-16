64c1b1bf8f86df062825f73b3b1b6ab771fdaa07
shanmh47 <9955667+shanmh47@users.noreply.github.com>
2022-06-16 15:23:20 +0800

    1d762a0d2d374d6e926815daf7ff1db7

--- imagestoretool/src/main/java/com/shanmh/imagestore/App.java
+++ imagestoretool/src/main/java/com/shanmh/imagestore/App.java
@@ -1,56 +1,17 @@
 package com.shanmh.imagestore;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
-/**
- * Hello world!
- */
 public class App {
 
     private static final List<String> IMAGE_EXTENSIONS = Arrays.asList("jpg", "jpeg", "png", "gif", "webp", "bmp", "ico");
     private static final String IMAGE_URL_PREFIX = "https://cdn.jsdelivr.net/gh/shanmh47/imgstore/";
 
-    private static String imageDirPath = "";
-
-    static {
-        if (imageDirPath == null || imageDirPath.length() == 0) {
-            try {
-                Process p = Runtime.getRuntime().exec("pwd");
-                byte[] bytes = readBytes(p.getInputStream());
-                imageDirPath = new String(bytes).replace("\n", "");
-            } catch (IOException ignore) {
-
-            }
-        }
-    }
-
-    private static byte[] readBytes(InputStream inputStream) throws IOException {
-        byte[] bytes = new byte[65536];
-        int offset = 0;
-
-        while (true) {
-            int readCount = inputStream.read(bytes, offset, bytes.length - offset);
-            if (readCount == -1) {
-                return Arrays.copyOfRange(bytes, 0, offset);
-            }
-
-            offset += readCount;
-            if (offset == bytes.length) {
-                byte[] newBytes = new byte[bytes.length * 3 / 2];
-                System.arraycopy(bytes, 0, newBytes, 0, bytes.length);
-                bytes = newBytes;
-            }
-        }
-    }
-
     public static void main(String[] args) {
-        File imageDir = new File(imageDirPath);
+        File imageDir = new File(args[0]);
         File[] images = imageDir.listFiles(file -> {
             return !file.isDirectory() && IMAGE_EXTENSIONS.contains(getFileExtension(file));
         });
---
+++ workspace/start.command
@@ -0,0 +1,2 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+java -jar ${DIR}/imagestoretool-1.0-SNAPSHOT-jar-with-dependencies.jar ${DIR}
